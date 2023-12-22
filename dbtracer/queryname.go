639f9b06af520b35c297b5d8b27e47a4b5d54bef
Amirsalar Safaei <amirsalar.safaei@divar.ir>
2023-12-22 18:59:09 +0330

    fix: fixed

---
+++ dbtracer/queryname.go
@@ -0,0 +1,13 @@
+package dbtracer
+
+import "regexp"
+
+// sqlc queries are declared after a sql command in the form of -- name: TheQueryName :type
+var queryNameRegex = regexp.MustCompile(`\A--\s+name:\s+(?P<name>\w+)`)
+
+func queryNameFromSQL(sql string) string {
+	if !queryNameRegex.MatchString(sql) {
+		return sql
+	}
+	return queryNameRegex.FindStringSubmatch(sql)[1]
+}
